737d5576e96b1de4eaf55ada7dc430390fc9fd04
Bharani Gnanasekaran <Bharani.Gnanasekaran@BestBuy.com>
2019-01-25 15:12:22 -0600

    Amazon S3 uploading Data changes

--- demo/src/main/java/com/example/demo/TestController.java
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -11,6 +11,7 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -68,11 +69,26 @@ public class TestController {
 
 
 
-    @RequestMapping(value = "/test/create", method= RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public void test3(@RequestBody JsonNode node){
+    @RequestMapping(value = "/test/create/{id}", method= RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void test3(@PathVariable String id, @RequestBody JsonNode node){
 
         if (node != null){
-            System.out.println(node.get("id").asText());
+            BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIAJNW2OMFZN2K63JOA", "9T8jcqg9wL+XftqJXZ6Z3Uh/OwBjbLPNAYqfK4+e");
+            final AmazonS3 s3 = AmazonS3Client.builder().withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                    .withRegion("us-east-1").build();
+            S3Object s3Object = new S3Object();
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            JsonNode txnNode = node.get("Transaction");
+            if(txnNode != null)
+            {
+                JsonNode headerNode = txnNode.get("header");
+                if(headerNode != null) {
+                    String uuid = headerNode.get("uuid").asText();
+                    String fileName = id + "/" + uuid + ".json";
+                    s3Object.setObjectMetadata(objectMetadata);
+                    s3.putObject("bharanibucket", fileName, node.asText());
+                }
+            }
         }
     }
 
