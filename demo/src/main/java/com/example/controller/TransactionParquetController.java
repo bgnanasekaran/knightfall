b7204ab987db4562144c61bbb451389a032cc78a
Bharani Gnanasekaran <Bharani.Gnanasekaran@BestBuy.com>
2019-02-25 17:14:59 -0600

    Amazon S3 uploading Data in parquet format - refactoring

--- demo/src/main/java/com/example/controller/TransactionParquetController.java
+++ demo/src/main/java/com/example/controller/TransactionParquetController.java
@@ -1,22 +1,10 @@
 package com.example.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.example.manager.S3ServiceManager;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericData;
-import org.apache.avro.generic.GenericRecord;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
-import org.apache.parquet.avro.AvroParquetReader;
-import org.apache.parquet.avro.AvroParquetWriter;
-import org.apache.parquet.avro.AvroReadSupport;
-import org.apache.parquet.hadoop.ParquetReader;
-import org.apache.parquet.hadoop.ParquetWriter;
-import org.apache.parquet.hadoop.metadata.CompressionCodecName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,24 +13,19 @@ public class TransactionParquetController {
 
     private Logger LOGGER = LoggerFactory.getLogger(TransactionParquetController.class);
 
-    private  String SCHEMA_TEMPLATE = "{\"namespace\": \"org.bharani.parquet\"," //Not used in Parquet, can put anything
-            + "\"type\": \"record\"," //Must be set as record
-            + "\"name\": \"testParquet\"," //Not used in Parquet, can put anything
-            + "\"fields\": ["
-            + " {\"name\": \"Transaction\",  \"type\": [\"string\", \"null\"]}"
-            + " ]}";
+    private S3ServiceManager serviceManager;
 
     @RequestMapping(value = "/online/transactions/{id}", method= RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public void test(@PathVariable String id, @RequestBody JsonNode node, @RequestHeader(value="access_key", required = false) String accessKey, @RequestHeader(value="secret_key", required = false) String secretKey) {
+    public void test(@PathVariable String id, @RequestBody JsonNode node, @RequestHeader(value="access_key", required = false) String accessKey, @RequestHeader(value="secret_key", required = false) String secretKey, @RequestHeader(value="bucket_name", required = false) String bucketName) {
         LOGGER.info("Creating online transactions ");
-        parquetWrite(id, node, accessKey, secretKey);
+        serviceManager.parquetCreate(id, node, accessKey, secretKey, bucketName);
     }
 
     @RequestMapping(value = "/online/transactions/{id}/{uuid}", method= RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public String test2(@PathVariable String id, @PathVariable String uuid,  @RequestHeader(value="access_key", required = false) String accessKey, @RequestHeader(value="secret_key", required = false) String secretKey){
+    public String test2(@PathVariable String id, @PathVariable String uuid,  @RequestHeader(value="access_key", required = false) String accessKey, @RequestHeader(value="secret_key", required = false) String secretKey, @RequestHeader(value="bucket_name", required = false) String bucketName){
         LOGGER.debug("Fetching Transxns of loyalty id " + id + " with uuid " + uuid );
-        JsonNode node =  parquetRead(id, uuid, accessKey, secretKey);
+        JsonNode node =  serviceManager.parquetGet(id, uuid, accessKey, secretKey, bucketName);
         if(node != null){
             LOGGER.info(node.toString());
             return node.toString();
@@ -50,94 +33,8 @@ public class TransactionParquetController {
         return null;
     }
 
-    private JsonNode parquetRead(String id, String uuid, String accessKey, String secretKey){
-
-        JsonNode node = null;
-        StringBuilder pathBuilder = getPath(id, uuid);
-
-        Path path = new Path(pathBuilder.toString());
-
-        Schema schema = new Schema.Parser().parse(SCHEMA_TEMPLATE);
-        Configuration conf = generateConfiguration(accessKey, secretKey);
-        AvroReadSupport.setRequestedProjection(conf, schema);
-        try {
-            ParquetReader<GenericRecord> parquetReader = AvroParquetReader.<GenericRecord>builder(path).withConf(conf).build();
-            GenericRecord genericRecord = parquetReader.read();
-            ObjectMapper objectMapper = new ObjectMapper();
-            node =  objectMapper.readTree(genericRecord.toString());
-        }
-        catch (Exception e){
-            LOGGER.error("Exception occured during read :: ", e);
-        }
-        return node;
+    @Autowired
+    public void setServiceManager(S3ServiceManager serviceManager) {
+        this.serviceManager = serviceManager;
     }
-
-    private void parquetWrite(String id, JsonNode node, String accessKey, String secretKey) {
-
-        if (node != null){
-            JsonNode txnNode = node.get("Transaction");
-            if(txnNode != null)
-            {
-                LOGGER.debug("adding Transxns of loyalty id " + id + " in progress" );
-                JsonNode headerNode = txnNode.get("header");
-                if(headerNode != null) {
-
-                    String uuid = headerNode.get("uuid").asText();
-                    StringBuilder pathBuilder = getPath(id, uuid);
-
-                    Schema.Parser parser = new Schema.Parser().setValidate(true);
-                    Schema avroSchema = parser.parse(SCHEMA_TEMPLATE);
-
-                    GenericData.Record record = new GenericData.Record(avroSchema);
-                    record.put("Transaction", txnNode.toString());
-
-                    try {
-                        Configuration conf = generateConfiguration(accessKey, secretKey);
-                        //Below are some other helpful settings
-                        //conf.set("fs.s3a.endpoint", "s3.amazonaws.com");
-                        //conf.set("fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider");
-                        //conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName()); // Not needed unless you reference the hadoop-hdfs library.
-                        //conf.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName()); // Uncomment if you get "No FileSystem for scheme: file" errors.
-
-                        Path path = new Path(pathBuilder.toString());
-
-                        try (ParquetWriter<GenericData.Record> writer = AvroParquetWriter.<GenericData.Record>builder(path)
-                                .withSchema(avroSchema)
-                                .withCompressionCodec(CompressionCodecName.SNAPPY)
-                                .withConf(conf)
-                                .build()) {
-                            writer.write(record);
-                        }
-                    } catch (Exception ex) {
-                        LOGGER.error("Exception occured :: ", ex);
-                    }
-                }
-            }
-        }
-
-    }
-
-    private StringBuilder getPath(String id, String uuid) {
-        StringBuilder pathBuilder = new StringBuilder();
-       // pathBuilder.append("s3a://bharanibucket/");
-        pathBuilder.append("s3a://bby-tgraph/");
-        pathBuilder.append(id);
-        pathBuilder.append("/");
-        pathBuilder.append(uuid);
-        pathBuilder.append(".");
-        pathBuilder.append("parquet");
-        return pathBuilder;
-    }
-
-    private Configuration generateConfiguration(String accessKey, String secretKey) {
-        Configuration conf = new Configuration();
-        if (StringUtils.isNotEmpty(accessKey) && StringUtils.isNotEmpty(secretKey)) {
-            conf.set("fs.s3a.access.key", accessKey);
-            conf.set("fs.s3a.secret.key", secretKey);
-        } else {
-            conf.set("fs.s3a.aws.credentials.provider", "com.amazonaws.auth.DefaultAWSCredentialsProviderChain");
-        }
-        return conf;
-    }
-
 }
--- demo/src/main/java/com/example/manager/S3ServiceManager.java
+++ demo/src/main/java/com/example/manager/S3ServiceManager.java
@@ -6,14 +6,25 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.parquet.avro.AvroParquetReader;
+import org.apache.parquet.avro.AvroParquetWriter;
+import org.apache.parquet.avro.AvroReadSupport;
+import org.apache.parquet.hadoop.ParquetReader;
+import org.apache.parquet.hadoop.ParquetWriter;
+import org.apache.parquet.hadoop.metadata.CompressionCodecName;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -23,6 +34,13 @@ import java.util.concurrent.atomic.AtomicReference;
 public class S3ServiceManager {
 
     private Logger LOGGER = LoggerFactory.getLogger(S3ServiceManager.class);
+
+    private  String PARQUET_SCHEMA_TEMPLATE = "{\"namespace\": \"org.bharani.parquet\"," //Not used in Parquet, can put anything
+            + "\"type\": \"record\"," //Must be set as record
+            + "\"name\": \"testParquet\"," //Not used in Parquet, can put anything
+            + "\"fields\": ["
+            + " {\"name\": \"Transaction\",  \"type\": [\"string\", \"null\"]}"
+            + " ]}";
 
     public void create(AmazonS3 s3, String id, JsonNode jsonNode, String fileFormat, String bucketName){
         if (jsonNode != null){
@@ -75,6 +93,91 @@ public class S3ServiceManager {
         }
         LOGGER.info(stringBuilder.toString());
         return stringBuilder.toString();
+    }
+
+
+    public JsonNode parquetGet(String id, String uuid, String accessKey, String secretKey, String bucketName){
+
+        StringBuilder pathBuilder = getPath(bucketName, id, uuid);
+
+        Path path = new Path(pathBuilder.toString());
+
+        Schema schema = new Schema.Parser().parse(PARQUET_SCHEMA_TEMPLATE);
+        Configuration conf = generateConfiguration(accessKey, secretKey);
+        AvroReadSupport.setRequestedProjection(conf, schema);
+        JsonNode node = null;
+        try {
+            ParquetReader<GenericRecord> parquetReader = AvroParquetReader.<GenericRecord>builder(path).withConf(conf).build();
+            GenericRecord genericRecord = parquetReader.read();
+            ObjectMapper objectMapper = new ObjectMapper();
+            node = objectMapper.readTree(genericRecord.toString());
+        }
+        catch (Exception e){
+            LOGGER.error("Exception occured during read :: ", e);
+        }
+        return node;
+    }
+
+    public void parquetCreate(String id, JsonNode node, String accessKey, String secretKey, String bucketName) {
+
+        if (node != null){
+            JsonNode txnNode = node.get("Transaction");
+            if(txnNode != null)
+            {
+                LOGGER.debug("adding Transxns of loyalty id " + id + " in progress" );
+                JsonNode headerNode = txnNode.get("header");
+                if(headerNode != null) {
+
+                    String uuid = headerNode.get("uuid").asText();
+                    StringBuilder pathBuilder = getPath(bucketName, id, uuid);
+
+                    Schema.Parser parser = new Schema.Parser().setValidate(true);
+                    Schema avroSchema = parser.parse(PARQUET_SCHEMA_TEMPLATE);
+
+                    GenericData.Record record = new GenericData.Record(avroSchema);
+                    record.put("Transaction", txnNode.toString());
+
+                    try {
+                        Configuration conf = generateConfiguration(accessKey, secretKey);
+                        Path path = new Path(pathBuilder.toString());
+                        try (ParquetWriter<GenericData.Record> writer = AvroParquetWriter.<GenericData.Record>builder(path)
+                                .withSchema(avroSchema)
+                                .withCompressionCodec(CompressionCodecName.SNAPPY)
+                                .withConf(conf)
+                                .build()) {
+                            writer.write(record);
+                        }
+                    } catch (Exception ex) {
+                        LOGGER.error("Exception occured :: ", ex);
+                    }
+                }
+            }
+        }
+
+    }
+
+    private StringBuilder getPath(String bucketName, String id, String uuid) {
+        StringBuilder pathBuilder = new StringBuilder();
+        pathBuilder.append("s3a://");
+        pathBuilder.append(bucketName);
+        pathBuilder.append("/");
+        pathBuilder.append(id);
+        pathBuilder.append("/");
+        pathBuilder.append(uuid);
+        pathBuilder.append(".");
+        pathBuilder.append("parquet");
+        return pathBuilder;
+    }
+
+    private Configuration generateConfiguration(String accessKey, String secretKey) {
+        Configuration conf = new Configuration();
+        if (StringUtils.isNotEmpty(accessKey) && StringUtils.isNotEmpty(secretKey)) {
+            conf.set("fs.s3a.access.key", accessKey);
+            conf.set("fs.s3a.secret.key", secretKey);
+        } else {
+            conf.set("fs.s3a.aws.credentials.provider", "com.amazonaws.auth.DefaultAWSCredentialsProviderChain");
+        }
+        return conf;
     }
 
     public void sparkWrite(){
